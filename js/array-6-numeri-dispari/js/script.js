db98ef041ec7c746dc4148bb0fe573745994c753
michelonmatteo <michelon.matteo89@gmail.com>
2021-03-23 12:22:39 +0100

    aggiunto esercizio array-6-numeri

---
+++ js/array-6-numeri-dispari/js/script.js
@@ -0,0 +1,16 @@
+// Crea un array vuoto. Chiedi per 6 volte all'utente 
+//di inserire un numero, se è dispari inseriscilo nell'array
+
+var mioArray = [];
+
+for ( i = 0; i < 6; i++ ) {
+    var numeroInserito = parseInt( prompt( 'Dimmi un numero' ) );
+
+    if ( numeroInserito % 2 != 0 ) {
+        console.log( numeroInserito );
+        mioArray.push( numeroInserito );
+    }
+}
+
+alert( 'Hai inserito i seguenti numeri dispari ' + mioArray );
+
--- js/numero-x-10/js/script.js
+++ js/numero-x-10/js/script.js
@@ -1,14 +1,12 @@
 //Il software deve chiedere per 10 volte all'utente di inserire un numero.
 //Il programma stampa la somma di tutti i numeri inseriti.
 
-var totaleNumeri = 0;
+var somma = 0;
 
 for ( var i = 0; i < 10; i++ ) {
     var numero = parseInt( prompt( 'Dimmi un numero' ) );
-    //console.log( numero );
-    totaleNumeri = totaleNumeri + numero;
-    //console.log( totaleNumeri );
+    somma += numero;
 }
 
-alert( "il totale dei numeri inseriti è " + totaleNumeri );
+alert( "il totale dei numeri inseriti è " + somma );
 
