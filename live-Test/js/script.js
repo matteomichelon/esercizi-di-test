113c5eec2608c6305bd52440ddbded97276bccae
Matteo Michelon <michelon.matteo89@gmail.com>
2021-04-28 11:53:23 +0200

    aggiornato con prepared folder

--- live-Test/js/script.js
+++ live-Test/js/script.js
@@ -1,89 +1,43 @@
+/* Document Ready */
+$( document ).ready( function () {
+
+    //====
+    /* write here if you use jQuery*/
+    //====
+
+} );
+/* end Document Ready */
+
 /* Vue */
 
 var app = new Vue(
     {
         el: '#root',
+
         /* DATA */
         data: {
-            pushText: '',
-            userFilterText: '',
-            todos: [
-                {
-                    text: 'Fare i compiti',
-                    filter: true,
-                    isDone: false
-                },
-                {
-                    text: 'Fare la spesa',
-                    filter: true,
-                    isDone: false
-                },
-                {
-                    text: 'Fare il bucato',
-                    filter: true,
-                    isDone: false
-                }
-            ],
+
         },
 
         /* METHODS */
         methods: {
 
-            // Aggiunge nuove stringhe all'array 
-            // di stringhe todos
-            pushTextOnArray () {
-                if ( this.pushText.length > 0 ) {
-                    const newObject = {
-                        text: this.pushText,
-                        filter: true,
-                        isDone: false
-                    }
-                    this.todos.push( newObject );
-                }
-                this.pushText = "";
-            },
+        },
 
-            // Aggiunge un colore alla stringa 
-            // quando il todo è done:true
-            thisDone ( index ) {
-                const object = this.todos[ index ];
-                object.isDone = !object.isDone;
-            },
-
-            // Rimuove stringa dall'array
-            // di stringa todos
-            deleteThisFromArray ( index ) {
-                const arrayTodos = this.todos;
-                arrayTodos.splice( index, 1 );
-            },
-
-            // Filtra stringa per l'utente
-            filterTodos () {
-                this.todos.forEach( ( element ) => {
-
-                    let valueSearch = element.text.toLowerCase().includes( this.userFilterText.toLowerCase() );
-
-                    if ( valueSearch ) {
-                        element.filter = true;
-                    } else {
-                        element.filter = false;
-                    }
-
+        /* MOUNTED */
+        mounted () {
+            axios
+                .get( 'url' )
+                .then( ( response ) => {
+                    const result = response.data;
                 } );
+        },
 
-            },
-
-            // Filtra todo da fare
-            filterIsDoneFalse () {
-                this.todos.forEach( ( element ) => {
-                    if ( element.isDone ) {
-                        element.filter = false
-                    }
-
-                } );
-            }
+        /* CREATED */
+        created(){
 
         }
+
     }
 );
 
